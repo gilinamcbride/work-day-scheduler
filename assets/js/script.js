51dd14ec39dd9120c6a846111c62a2db28e63221
Gilina McBride <gilinamcbride@gmail.com>
2022-05-12 22:04:25 -0400

    add date and ticking time to header

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,27 +2,61 @@
 var itemDescription = $(".description");
 var saveButton = $("#saveButton");
 var header = $(".jumbotron");
-var itemText = [];
+// var itemText = [];
+var today = moment().format("MMMM DD, YYYY");
 
-//add current date to jumbotron
-function date() {
-  var today = new Date();
-  header.append(today);
-}
-//audit to refresh page and check for time
-//past activities turn red
-//current activities turn green
+var showDate = function () {
+  var date = $("<p>").addClass("date").text(today);
+  header.append(date);
+};
+showDate();
 
-//load items fx
-var loadItems = function () {
-  tasks = JSON.parse(localStorage.getItem("tasks"));
+var showTime = function () {
+  var time = moment().format("hh:mm A");
+  var getTime = $("<p>").addClass("date").text(time);
+  header.append(getTime);
+  setInterval(function () {
+    getTime.text("");
+    showTime();
+  }, 60000);
+};
+showTime();
+
+//TODO: audit to refresh page and check for time of day
+var auditSchedule = function (itemDescription) {
+  var date = today;
+  var time = moment(date, "L").set("hour", 23);
+  //TODO: past activities turn grey --> moment time + x hours to get past hour of list
+  if (moment().isAfter(time)) {
+    $(itemDescription).addClass("past");
+  }
+  //TODO: current activities turn green ---> moment time + x hours to get to exact hour of list
+  if (moment().isBefore(time)) {
+    $(itemDescription).addClass("future");
+  }
+  //TODO: current activities turn red ---> moment time + x hours to get to exact hour of list
+  if (moment(time)) {
+    $(itemDescription).addClass("present");
+  }
+  setInterval(function () {
+    $(".description").each(function (index, el) {
+      auditSchedule(el);
+    });
+  }, 1000 * 60 * 30);
 };
 
-//save items fx
+//TODO: load items fx
+// var loadItems = function () {
+//   tasks = JSON.parse(localStorage.getItem("tasks"));
+// };
+
+//TODO: save items fx
 var saveItems = function () {
-  localStorage.setItem("tasks", JSON.stringify(tasks));
+  console.log("this works");
+  var itemText = $(this).find("textarea").text().trim();
+  localStorage.setItem("itemText", JSON.stringify(itemText));
 };
 saveButton.on("click", saveItems);
 
-//load items for the first time
+//TODO: load items for the first time
 //loadItems();
