85362780cf6ecb7690275e20b4db103c5d435681
Gilina McBride <gilinamcbride@gmail.com>
2022-05-13 17:07:17 -0400

    create fx to change colors based on time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,8 @@
 //variables on page needed
-var itemDescription = $(".description");
 var saveButton = $("#saveButton");
 var header = $(".jumbotron");
-// var itemText = [];
 var today = moment().format("MMMM DD, YYYY");
+var time = moment().format("hh:mm A");
 
 var showDate = function () {
   var date = $("<p>").addClass("date").text(today);
@@ -11,39 +10,70 @@ var showDate = function () {
 };
 showDate();
 
-var showTime = function () {
-  var time = moment().format("hh:mm A");
-  var getTime = $("<p>").addClass("date").text(time);
-  header.append(getTime);
-  setInterval(function () {
-    getTime.text("");
-    showTime();
-  }, 60000);
-};
-showTime();
+//TODO: change colors with time:
+function colorTime() {
+  var currentDate = new Date();
 
-//TODO: audit to refresh page and check for time of day
-var auditSchedule = function (itemDescription) {
-  var date = today;
-  var time = moment(date, "L").set("hour", 23);
-  //TODO: past activities turn grey --> moment time + x hours to get past hour of list
-  if (moment().isAfter(time)) {
-    $(itemDescription).addClass("past");
-  }
-  //TODO: current activities turn green ---> moment time + x hours to get to exact hour of list
-  if (moment().isBefore(time)) {
-    $(itemDescription).addClass("future");
-  }
-  //TODO: current activities turn red ---> moment time + x hours to get to exact hour of list
-  if (moment(time)) {
-    $(itemDescription).addClass("present");
+  switch (currentDate.getHours()) {
+    case 9:
+      $("#hour9").addClass("present");
+      $("#hour10").addClass("future");
+      $("#hour11").addClass("future");
+      $("#hour12").addClass("future");
+      $("#hour1").addClass("future");
+      $("#hour2").addClass("future");
+      $("#hour3").addClass("future");
+      $("#hour4").addClass("future");
+      $("#hour5").addClass("future");
+      break;
+    case 10:
+      $("#hour9").removeClass("present");
+      $("#hour10").removeClass("future");
+      $("#hour10").addClass("present");
+      break;
+    case 11:
+      $("#hour10").removeClass("present");
+      $("#hour11").removeClass("future");
+      $("#hour11").addClass("present");
+      break;
+    case 12:
+      $("#hour11").removeClass("present");
+      $("#hour12").removeClass("future");
+      $("#hour12").addClass("present");
+      break;
+    case 13:
+      $("#hour12").removeClass("present");
+      $("#hour1").removeClass("future");
+      $("#hour1").addClass("present");
+      break;
+    case 14:
+      $("#hour1").removeClass("present");
+      $("#hour2").removeClass("future");
+      $("#hour2").addClass("present");
+      break;
+    case 15:
+      $("#hour2").removeClass("present");
+      $("#hour3").removeClass("future");
+      $("#hour3").addClass("present");
+      break;
+    case 16:
+      $("#hour3").removeClass("present");
+      $("#hour4").removeClass("future");
+      $("#hour4").addClass("present");
+      break;
+    case 17:
+      $("#hour4").removeClass("present");
+      $("#hour5").removeClass("future");
+      $("#hour5").addClass("present");
+      break;
+    default:
+      break;
   }
   setInterval(function () {
-    $(".description").each(function (index, el) {
-      auditSchedule(el);
-    });
-  }, 1000 * 60 * 30);
-};
+    colorMinutes();
+  }, 1000 * 60 * 60);
+}
+colorTime();
 
 //TODO: load items fx
 // var loadItems = function () {
@@ -60,3 +90,84 @@ saveButton.on("click", saveItems);
 
 //TODO: load items for the first time
 //loadItems();
+
+// var showTime = function () {
+
+//   var getTime = $("<p>").addClass("date").text(time);
+//   header.append(getTime);
+// };
+// setInterval(function () {
+//   header.remove(".date");
+//   showTime();
+// }, 60000);
+// showTime();
+
+//TODO: audit to refresh page and check for time of day
+// var auditSchedule = function () {
+//   var date = today;
+
+//   function hour9() {
+//     $("#hour9").removeClass("past");
+//     //TODO: present activities
+//     if (moment(date, "L").add(9, "hour")) {
+//       console.log("equal");
+//       $("#hour9").addClass("present");
+//     }
+//     //TODO: future activities
+//     if (moment(date, "L").add(10, "hour")) {
+//       console.log("before");
+//       $("hour9").addClass("future");
+//     }
+//     //TODO: past activities
+//     else {
+//       console.log("after");
+//       $("hour9").addClass("past");
+//     }
+//   }
+//   hour9();
+
+//   function hour1() {
+//     $("#hour2").removeClass("past");
+//     //TODO: present activities
+//     if (moment(date, "L").add(14, "hour")) {
+//       console.log("equal");
+//       $("#hour2").addClass("present");
+//     }
+//     //TODO: future activities
+//     if (moment(date, "L").add(13, "hour")) {
+//       console.log("before");
+//       $("hour2").addClass("future");
+//     }
+//     //TODO: past activities
+//     else {
+//       console.log("after");
+//       $("hour2").addClass("past");
+//     }
+//   }
+//   hour1();
+
+//   function hour2() {
+//     $("#hour3").removeClass("past");
+//     //TODO: present activities
+//     if (moment(date, "L").add(15, "hour")) {
+//       console.log("equal");
+//       $("#hour3").addClass("present");
+//     }
+//     //TODO: future activities
+//     else if (moment(date, "L").add(14, "hour")) {
+//       console.log("before");
+//       $("hour3").addClass("future");
+//     }
+//     //TODO: past activities
+//     else {
+//       console.log("after");
+//       $("hour3").addClass("past");
+//     }
+//   }
+//   hour2();
+
+//   setInterval(function () {
+//     auditSchedule();
+//   }, 1000 * 60 * 60);
+// };
+// auditSchedule();
